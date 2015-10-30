a724dda4b53ae78644e51956a46a8ac816650145
Noah Bannister <noahabannister@gmail.com>
2015-10-30 10:57:36 -0400

    Initial Push

---
+++ Assets/ModelSwap.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelSwap : MonoBehaviour {
+
+	public GameObject[] models;
+	private int current = 0;
+
+	public void Cycle (int step) {
+		models [current].SetActive (false);
+		current = (current + step + models.Length) % models.Length;
+		models [current].SetActive (true);
+		
+	}
+}
---
+++ Assets/Screencapture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screencapture : MonoBehaviour {
+	public Canvas canvas;
+	public void TakePicture () {
+		canvas.gameObject.SetActive (false);
+
+		StartCoroutine (screenshotDelay());
+	}
+	IEnumerator screenshotDelay(){
+		yield return new WaitForEndOfFrame ();
+		yield return new WaitForEndOfFrame ();
+		string time = System.DateTime.Now.ToString ("yyyy-MM-dd_HH-mm-ss");
+		Application.CaptureScreenshot("MaskAR"+time+".png");
+		canvas.gameObject.SetActive (true);
+	}
+}
